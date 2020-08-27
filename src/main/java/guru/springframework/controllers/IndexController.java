6cdde5f10b7848b26ae9795e272999d0f6871d0a
papashep <melvyn.shepherd@outlook.com>
2020-08-27 17:38:15 +0100

    Lecture 140 Loading categories and unit-of-measures.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by Melvyn on 26/Aug/2020
@@ -10,8 +16,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController (CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription ("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription ("Teaspoon");
+//        System.out.println ("Cat Id is : " + categoryOptional.get ().getId ());
+//        System.out.println ("Uom Id is : " + unitOfMeasureOptional.get ().getId ());
+
+        System.out.println("Cat ID is: " + categoryOptional
+                .map(category -> categoryOptional.get().getId())
+                .orElse(-1L));
+
+        System.out.println("Uom ID is: " + unitOfMeasureOptional
+                .map(unitOfMeasure -> unitOfMeasureOptional.get().getId())
+                .orElse(-1L));
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,9 +3,13 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Melvyn on 26/Aug/2020
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 
 }
