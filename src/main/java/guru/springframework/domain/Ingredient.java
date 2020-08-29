f0ea8fa750a58f545f0b724a5f9c8f80f9f48734
papashep <melvyn.shepherd@outlook.com>
2020-08-29 10:57:21 +0100

    Lecture 143. Working Ok after applying Pro-Tips

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -25,14 +25,18 @@ public class Ingredient {
     public Ingredient () {
     }
 
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
         this.recipe = recipe;
     }
-
-
 
     public Long getId () {
         return id;
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -42,107 +42,114 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Long getId () {
+    public Long getId() {
         return id;
     }
 
-    public void setId (Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getDescription () {
+    public String getDescription() {
         return description;
     }
 
-    public void setDescription (String description) {
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public Integer getPrepTime () {
+    public Integer getPrepTime() {
         return prepTime;
     }
 
-    public void setPrepTime (Integer prepTime) {
+    public void setPrepTime(Integer prepTime) {
         this.prepTime = prepTime;
     }
 
-    public Integer getCookTime () {
+    public Integer getCookTime() {
         return cookTime;
     }
 
-    public void setCookTime (Integer cookTime) {
+    public void setCookTime(Integer cookTime) {
         this.cookTime = cookTime;
     }
 
-    public Integer getServings () {
+    public Integer getServings() {
         return servings;
     }
 
-    public void setServings (Integer servings) {
+    public void setServings(Integer servings) {
         this.servings = servings;
     }
 
-    public String getSource () {
+    public String getSource() {
         return source;
     }
 
-    public void setSource (String source) {
+    public void setSource(String source) {
         this.source = source;
     }
 
-    public String getUrl () {
+    public String getUrl() {
         return url;
     }
 
-    public void setUrl (String url) {
+    public void setUrl(String url) {
         this.url = url;
     }
 
-    public String getDirections () {
+    public String getDirections() {
         return directions;
     }
 
-    public void setDirections (String directions) {
+    public void setDirections(String directions) {
         this.directions = directions;
     }
 
-    public Set<Ingredient> getIngredients () {
-        return ingredients;
-    }
-
-    public void setIngredients (Set<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public Byte[] getImage () {
+    public Byte[] getImage() {
         return image;
     }
 
-    public void setImage (Byte[] image) {
+    public void setImage(Byte[] image) {
         this.image = image;
     }
 
-    public Difficulty getDifficulty () {
-        return difficulty;
-    }
-
-    public void setDifficulty (Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public Notes getNotes () {
+    public Notes getNotes() {
         return notes;
     }
 
-    public void setNotes (Notes notes) {
+    public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
-    public Set<Category> getCategories () {
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
         return categories;
     }
 
-    public void setCategories (Set<Category> categories) {
+    public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
 }
